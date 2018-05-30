5d17d1dc0907eb0a961dde9c6238e0036d87d03b
Bill Fienberg <billfienberg@users.noreply.github.com>
2018-05-30 12:45:26 -0500

    Add video overlay

--- src/Player.js
+++ src/Player.js
@@ -11,20 +11,31 @@ const Player = (props = {}) => (
   />
 )
 
+const RelativePositionWrapper = styled.div`
+  position: relative;
+  padding-top: 56.25%;
+`
+
 const AbsolutelyPositionedPlayer = styled(Player)`
   position: absolute;
   top: 0;
   left: 0;
 `
 
-const RelativePositionWrapper = styled.div`
-  position: relative;
-  padding-top: 56.25%;
+const Overlay = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  background: ${props => props.overlayColor};
+  opacity: ${props => props.opacity};
 `
 
 const ResponsiveStyledPlayer = (props = {}) => (
-  <RelativePositionWrapper>
+  <RelativePositionWrapper {...props}>
     <AbsolutelyPositionedPlayer {...props} />
+    <Overlay {...props} />
   </RelativePositionWrapper>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { render } from "react-dom";
-import Player from "./Player";
+import React from 'react'
+import { render } from 'react-dom'
+import Player from './Player'
 
 const App = () => {
   // https://github.com/CookPete/react-player#props
@@ -9,12 +9,18 @@ const App = () => {
     muted: true,
     controls: false,
     loop: true,
-    url: "https://www.youtube.com/watch?v=Dx-XMAJ1mwU"
-  };
+    url: 'https://www.youtube.com/watch?v=Dx-XMAJ1mwU',
+  }
+
+  const overlayProps = {
+    opacity: 0.5,
+    overlayColor: 'black',
+  }
+
   return (
     <div>
-      <Player {...playerProps} />
+      <Player {...playerProps} {...overlayProps} />
     </div>
-  );
-};
-render(<App />, document.getElementById("root"));
+  )
+}
+render(<App />, document.getElementById('root'))
